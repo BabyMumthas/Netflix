6f6ce4b73a2c224852d578f23fa65b2e7121fcdd
Baby mumthas <babymumthas808@gmail.com>
2025-01-30 18:12:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+// eslint-disable-next-line no-unused-vars
+import NavBar from './components/NavBar/NavBar'
+import './App.css'
+function App() {
+  return (
+    <div className="App">
+      <NavBar/> 
+    </div>
+  )
+}
+
+export default App
